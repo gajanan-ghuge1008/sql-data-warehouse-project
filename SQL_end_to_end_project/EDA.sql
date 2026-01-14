9b482621b7f20e0bb5356c856efe4609c3df29cb
Gajanan Ghuge <gajananghuge1008@gmail.com>
2026-01-14 12:51:40 +0530

    Update EDA.sql

--- SQL_end_to_end_project/EDA.sql
+++ SQL_end_to_end_project/EDA.sql
@@ -1,4 +1,11 @@
 -- Exploratory Data Analysis
+
+-- Here we are jsut going to explore the data and find trends or patterns or anything interesting like outliers
+
+-- normally when you start the EDA process you have some idea of what you're looking for
+
+-- with this info we are just going to look around and see what we find!
+
 SELECT * 
 FROM sql_data_cleaning.layoffs_cleaned;
 
